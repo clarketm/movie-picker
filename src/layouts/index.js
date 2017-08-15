49a5895511752147e6a80fd3c584d4a765859651
Travis Clarke <travis.m.clarke@gmail.com>
2017-08-14 18:47:41 -0700

    Replace inline styles w/ css declarations

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,64 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-// import './index.css'
 import './css/style.css'
 
 const Header = () => {
     return (
-        <div
-            style={{
-                background: 'rebeccapurple',
-                marginBottom: '1.45rem',
-            }}
-        >
-            <div
-                style={{
-                    margin: '0 auto',
-                    maxWidth: 960,
-                    padding: '1.45rem 1.0875rem',
-                }}
-            >
-                <h1 style={{margin: 0}}>
-                    <Link
-                        to="/"
-                        style={{
-                            color: 'white',
-                            textDecoration: 'none',
-                        }}
-                    > Movie Picker
-                    </Link>
-                </h1>
-            </div>
-        </div>
+        <header id="hero">
+            <h1>Netflix Movie Picker</h1>
+        </header>
     );
 };
 
 const TemplateWrapper = ({children}) => {
 
     return (
-        <div>
+        <main>
             <Helmet
-                title="Movie Picker"
+                title="Netflix Movie Picker"
                 meta={[
-                    {name: 'description', content: 'Sample'},
-                    {name: 'keywords', content: 'sample, something'},
+                    {name: 'description', content: 'Netflix Movie Picker'},
+                    {name: 'keywords', content: 'travismclarke, netflix, gatsbyjs, movie, picker, movie-picker, roulette, netflix roulette'},
                 ]}
             />
             <Header/>
-            <div
-                style={{
-                    margin: '0 auto',
-                    maxWidth: 960,
-                    padding: '0px 1.0875rem 1.45rem',
-                    paddingTop: 0,
-                }}
-            >
+            <section id="main" className="container">
                 {children()}
-            </div>
-        </div>
+            </section>
+        </main>
     )
 };
 
