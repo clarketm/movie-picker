945dd2ee735d6635621ee5270da4ca6c884ae1f2
Travis Clarke <travis.m.clarke@gmail.com>
2018-08-29 01:48:08 -0700

    maximum-scall viewport content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -40,7 +40,12 @@ const TemplateWrapper = ({ children }) => {
           {
             name: "keywords",
             content: "travismclarke, netflix, gatsbyjs, movie, picker, movie-picker, roulette, netflix roulette"
+          },
+          {
+            name: "viewport",
+            content: "width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
           }
+
         ]}
       />
       <Header />
